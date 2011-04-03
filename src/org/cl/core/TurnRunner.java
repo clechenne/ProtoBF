8a1c0cadfeb55aaa41f45327959f73c6fd668293
unknown <christophe@.(none)>
2011-04-03 14:38:46 +0200

    TurnRunnerTest ok

--- src/org/cl/core/TurnRunner.java
+++ src/org/cl/core/TurnRunner.java
@@ -1,10 +1,16 @@
 package org.cl.core;
 
+import org.cl.model.Game;
+
 public class TurnRunner {
+	private Game game;
+	
+	public TurnRunner(Game game) {
+		this.game = game;
+	}
 
 	public void end() {
-		// TODO Auto-generated method stub
-		
+		game.turn++;	
 	}
 
 }
--- tests/org/cl/core/TurnRunnerTest.java
+++ tests/org/cl/core/TurnRunnerTest.java
@@ -1,15 +1,25 @@
 package org.cl.core;
 
+import junit.framework.Assert;
+
+import org.cl.model.Game;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 
 public class TurnRunnerTest {
 	
+	private static Game game;
+	
+	@BeforeClass public static void init() {
+		game = new Game();
+		game.turn = 1;
+	}
+	
 	@Test
 	public void endTurn() {
-		TurnRunner tr = new TurnRunner();
-		
+		TurnRunner tr = new TurnRunner(game);
 		tr.end();
-		
+		Assert.assertEquals("Bad turn", 2, game.turn);
 	}
 }
